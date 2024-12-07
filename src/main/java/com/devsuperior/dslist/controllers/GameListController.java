74e4e34aa85644b1ebbf4f96ecd5579c91842ccd
Ruan Rodrigues <rupriwell123@outlook.com>
2024-12-07 06:56:36 -0300

    Get By List

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -20,11 +20,18 @@ public class GameListController {
     @Autowired(required = true)
     private GameListService gameListService;
 
+    @Autowired(required = true)
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll(){
         List<GameListDTO> result = gameListService.findAll();
         return result;
     }
-
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        List<GameMinDTO> result =  gameService.findByList(listId);
+        return result;
+    }
 
 }
